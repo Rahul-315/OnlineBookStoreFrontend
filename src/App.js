8e866dd9567ec8d68f7b1e375f1642988cfb88bd
Rahul_Pulluri <pullurirahul315@gmail.com>
2026-01-07 23:11:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AddBook from "./components/AddBook";
 import SearchBook from "./components/SearchBook";
 import UpdateBook from "./components/UpdateBook";
 import BookList from "./components/BookList";
-import Navbar from "./components/Navbar";
+import Navbar from "./Navbar";
 import "./styles/App.css";
 
 function App() {
